6d6414aaa38152338e376cd3eee6d3475a7d2c9e
Bhavesh Gadling <bhaveshgadling2025@gmail.com>
2026-04-21 15:00:28 +0530

    Time: 3 ms (76.76%) | Memory: 7.7 MB (99.66%) - LeetSync

---
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int dfs(vector<int>& freq) {
+        int count = 0;
+
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] == 0) continue;
+
+            // choose
+            count++; 
+            freq[i]--;
+
+            // explore
+            count += dfs(freq);
+
+            // backtrack
+            freq[i]++;
+        }
+
+        return count;
+    }
+
+    int numTilePossibilities(string tiles) {
+        vector<int> freq(26, 0);
+
+        for (char c : tiles) {
+            freq[c - 'A']++;
+        }
+
+        return dfs(freq);
+    }
+};
