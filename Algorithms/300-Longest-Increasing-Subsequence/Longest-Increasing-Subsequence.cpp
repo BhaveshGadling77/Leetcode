b3bdc9d11e3433fda46d4a81f61f31a39f96e627
Bhavesh Gadling <bhaveshgadling2025@gmail.com>
2026-06-22 11:40:56 +0530

    Time: 146 ms (36.02%) | Memory: 14.5 MB (38.1%) - LeetCommit

--- Algorithms/300-Longest-Increasing-Subsequence/Longest-Increasing-Subsequence.cpp
+++ Algorithms/300-Longest-Increasing-Subsequence/Longest-Increasing-Subsequence.cpp
@@ -21,19 +21,34 @@ public:
 
         //tabulation
 
-        vector<vector<int>>dp(n +1, vector<int>(n + 1, 0));
+        // vector<vector<int>>dp(n +1, vector<int>(n + 1, 0));
 
+        // for (int indx = n-1; indx >= 0; indx--) {
+        //     for (int prev = indx - 1; prev >= -1; prev--) {
+        //         int notTake = dp[indx + 1][prev+1];
+        //         int take = INT_MIN;
+        //         if (prev == -1 || nums[indx] > nums[prev])
+        //             take = 1 + dp[indx + 1][indx + 1];
+                
+        //         dp[indx][prev +1] = max(notTake, take);
+        //     }
+        // }
+
+        //space optimization.
+        vector<int>after(n + 1, 0);
+        vector<int>curr(n + 1, 0);
         for (int indx = n-1; indx >= 0; indx--) {
             for (int prev = indx - 1; prev >= -1; prev--) {
-                int notTake = dp[indx + 1][prev+1];
+                int notTake = after[prev+1];
                 int take = INT_MIN;
                 if (prev == -1 || nums[indx] > nums[prev])
-                    take = 1 + dp[indx + 1][indx + 1];
+                    take = 1 + after[indx + 1];
                 
-                dp[indx][prev +1] = max(notTake, take);
+                curr[prev +1] = max(notTake, take);
             }
+            after = curr;
         }
         
-        return dp[0][0];
+        return curr[0];
     }
 };
