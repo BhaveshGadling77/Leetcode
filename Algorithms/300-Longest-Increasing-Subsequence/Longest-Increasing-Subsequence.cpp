373ffc5ec10d9c766fd0add1c5f4d85500a876a3
Bhavesh Gadling <bhaveshgadling2025@gmail.com>
2026-06-22 11:37:10 +0530

    Time: 266 ms (32.42%) | Memory: 296.4 MB (24.89%) - LeetCommit

--- Algorithms/300-Longest-Increasing-Subsequence/Longest-Increasing-Subsequence.cpp
+++ Algorithms/300-Longest-Increasing-Subsequence/Longest-Increasing-Subsequence.cpp
@@ -16,7 +16,24 @@ public:
     }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>>dp(n, vector<int>(n + 1, -1));
-        return helper(0, -1, nums, dp);
+        // vector<vector<int>>dp(n, vector<int>(n + 1, -1));
+        // return helper(0, -1, nums, dp);
+
+        //tabulation
+
+        vector<vector<int>>dp(n +1, vector<int>(n + 1, 0));
+
+        for (int indx = n-1; indx >= 0; indx--) {
+            for (int prev = indx - 1; prev >= -1; prev--) {
+                int notTake = dp[indx + 1][prev+1];
+                int take = INT_MIN;
+                if (prev == -1 || nums[indx] > nums[prev])
+                    take = 1 + dp[indx + 1][indx + 1];
+                
+                dp[indx][prev +1] = max(notTake, take);
+            }
+        }
+        
+        return dp[0][0];
     }
 };
