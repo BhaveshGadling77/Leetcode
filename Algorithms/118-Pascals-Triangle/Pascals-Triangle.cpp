f571c7047ca96eca37efb6a7f668b23c8a8c2faa
Bhavesh Gadling <bhaveshgadling2025@gmail.com>
2026-07-08 22:57:42 +0530

    Time: 0 ms (100%) | Memory: 9.6 MB (90.11%) - LeetCommit

--- Algorithms/118-Pascals-Triangle/Pascals-Triangle.cpp
+++ Algorithms/118-Pascals-Triangle/Pascals-Triangle.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    vector <int> generateRow(int row) {
-        long long ans = 1;
-        vector <int> ansRow;
-        ansRow.push_back(1);
+    vector<int>generateRow(int row) {
+        vector<int>ans;
+        int res = 1;
+        ans.push_back(1);
         for (int col = 1; col < row; col++) {
-                ans = ans * (row - col);
-                ans = ans / col;
-                ansRow.push_back(ans);
+                res = res * (row - col);
+                res = res / col;
+                ans.push_back(res);
         }
-        return ansRow;
-    }
 
+        return ans;
+    }
     vector<vector<int>> generate(int numRows) {
-        vector <vector<int>> ans;
+        vector<vector<int>>ans;
+        
         for (int i = 1; i <= numRows; i++) {
             ans.push_back(generateRow(i));
-        } 
+        }
         return ans;
     }
 };
