f10e55d9e91c6c5ee7107a8d4ac5f2f98f0d7494
Bhavesh Gadling <bhaveshgadling2025@gmail.com>
2026-07-20 17:10:51 +0530

    Time: 0 ms (100%) | Memory: 19.2 MB (17.4%) - LeetCommit

--- Algorithms/1386-Shift-2D-Grid/Shift-2D-Grid.cpp
+++ Algorithms/1386-Shift-2D-Grid/Shift-2D-Grid.cpp
@@ -8,19 +8,21 @@ public:
             for (int j = 0; j < m; j++)
                 fg.push_back(grid[i][j]);
         k = k % fg.size();
-        //reverse(fg.begin(), );
-        vector<int> temp;
-        for(int i=fg.size()-k;i<fg.size();i++){
-            temp.push_back(fg[i]);
-        }
-        for(int z=0;z<fg.size()-k;z++){
-            temp.push_back(fg[z]);
-        }
+        reverse(fg.begin(), fg.end());
+        reverse(fg.begin(), fg.begin() + k);
+        reverse(fg.begin() + k, fg.end());
+        // vector<int> temp;
+        // for(int i=fg.size()-k;i<fg.size();i++){
+        //     temp.push_back(fg[i]);
+        // }
+        // for(int z=0;z<fg.size()-k;z++){
+        //     temp.push_back(fg[z]);
+        // }
         
         int s = 0;
         for (int i = 0; i < n; i++)
             for (int j = 0; j < m; j++)
-                grid[i][j] = temp[s++];
+                grid[i][j] = fg[s++];
         
         return grid;
     }
