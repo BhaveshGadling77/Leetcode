47de0847b41c89307a3f749e97bb5f45e27fbc8e
Bhavesh Gadling <bhaveshgadling2025@gmail.com>
2026-08-04 09:59:14 +0530

    Time: 1 ms (60.57%) | Memory: 33.8 MB (17.56%) - LeetCommit

---
+++ Algorithms/4107-Find-Missing-Elements/Find-Missing-Elements.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> findMissingElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>ans;
+        map<int,int>mpp;
+        int mini = INT_MAX;
+        int maxi = INT_MIN;
+        for (int i = 0; i < n; i++) {
+            mpp[nums[i]]++;
+            maxi = max(nums[i], maxi);
+            mini = min(nums[i], mini);
+        }
+
+        
+        for (int i = mini; i <= maxi; i++) {
+            if (mpp.find(i) == mpp.end())
+                ans.push_back(i);
+        }
+        return ans;
+    }
+};
