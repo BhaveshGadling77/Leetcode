48f14f8b1db5d93cc3e18a20e1402deaaea67d23
Bhavesh Gadling <bhaveshgadling2025@gmail.com>
2026-08-05 19:47:14 +0530

    Time: 806 ms (5.52%) | Memory: 324.3 MB (49.66%) - LeetCommit

---
+++ Algorithms/3561-Remove-Methods-From-Project/Remove-Methods-From-Project.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
+
+        vector<vector<int>> adj(n);
+
+        for (auto &e : invocations)
+            adj[e[0]].push_back(e[1]);
+
+        vector<bool> suspicious(n, false);
+
+        function<void(int)> dfs = [&](int u) {
+            suspicious[u] = true;
+
+            for (int v : adj[u]) {
+                if (!suspicious[v])
+                    dfs(v);
+            }
+        };
+
+        dfs(k);
+
+        // Check if any safe node calls a suspicious node
+        for (auto &e : invocations) {
+            int u = e[0];
+            int v = e[1];
+
+            if (!suspicious[u] && suspicious[v]) {
+                vector<int> ans;
+                for (int i = 0; i < n; i++)
+                    ans.push_back(i);
+                return ans;
+            }
+        }
+
+        vector<int> ans;
+        for (int i = 0; i < n; i++) {
+            if (!suspicious[i])
+                ans.push_back(i);
+        }
+
+        return ans;
+    }
+};
