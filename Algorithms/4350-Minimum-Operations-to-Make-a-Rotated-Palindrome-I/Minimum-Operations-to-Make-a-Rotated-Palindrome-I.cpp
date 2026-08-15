64285f6a94b2c2cfa9646e915e2f262b7ee56941
Bhavesh Gadling <bhaveshgadling2025@gmail.com>
2026-08-15 23:33:31 +0530

    Time: 111 ms (95.97%) | Memory: 10.7 MB (78.81%) - LeetCommit

---
+++ Algorithms/4350-Minimum-Operations-to-Make-a-Rotated-Palindrome-I/Minimum-Operations-to-Make-a-Rotated-Palindrome-I.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int minOperations(string s) {
+        int n = s.size();
+        int ans = INT_MAX;
+        for (int i = 0; i < n; i++) {
+            int l = 0;
+            int r = n - 1;
+            int cnt = i;
+            while (l < r) {
+                if (s[l] == s[r]) {
+                    l++;
+                    r--;
+                    continue;
+                }
+                int ops = abs(s[l] - s[r]);
+
+                cnt += min(ops, 26-ops);
+                l++;
+                r--;
+            }
+            ans = min(ans, cnt);
+            rotate(s.begin(), s.begin() + 1, s.end());
+        }
+        int l = 0;
+            int r = n - 1;
+            int cnt = 0;
+            while (l < r) {
+                if (s[l] == s[r]) {
+                    l++;
+                    r--;
+                    continue;
+                }
+                int ops = abs(s[l] - s[r]);
+
+                cnt += min(ops, 26-ops);
+                l++;
+                r--;
+            }
+            ans = min(ans, cnt);
+        return ans;
+    }
+};
