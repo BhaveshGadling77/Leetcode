4abb2f219b279f850e8daf116ee9e70c1696d1d4
Bhavesh Gadling <bhaveshgadling2025@gmail.com>
2026-08-30 10:41:12 +0530

    Time: 0 ms (100%) | Memory: 92.2 MB (22.14%) - LeetCommit

---
+++ Algorithms/2212-Removing-Minimum-and-Maximum-From-Array/Removing-Minimum-and-Maximum-From-Array.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int minimumDeletions(vector<int>& nums) {
+        int n = nums.size();
+        int minIndx = 0;
+        int maxIndx = 0;
+        int mini = INT_MAX;
+        int maxi = INT_MIN;
+        // find min and max index;
+        for (int i = 0; i < n; i++) {
+            if (mini > nums[i]) {
+                mini = nums[i];
+                minIndx = i;
+            }
+            if (maxi < nums[i]) {
+                maxi = nums[i];
+                maxIndx = i;
+            }
+        }
+        cout << maxi << " " << mini << endl;
+        cout << minIndx << " " << maxIndx << " " << n;
+        int ans = 0;
+        if (minIndx <= n / 2 && maxIndx <= n / 2) {
+            ans = max(minIndx + 1, maxIndx + 1);
+        } else if (minIndx < n / 2 && maxIndx > n / 2) {
+            ans = min({minIndx + 1 + (n - maxIndx),
+                        maxIndx + 1,
+                        n - minIndx}) ;
+        } else if (minIndx > n / 2 && maxIndx < n / 2) {
+            ans = min({maxIndx + 1 + (n - minIndx),
+                        minIndx + 1, 
+                        n - maxIndx});
+        } else {
+            minIndx = n - minIndx;
+            maxIndx = n - maxIndx;
+            ans = max(minIndx, maxIndx);
+        }
+        return ans;
+    }
+};
