1e0256f5cf4abcf10bb512b54af0e9a9e88fade2
Bhavesh Gadling <bhaveshgadling2025@gmail.com>
2026-09-14 18:49:11 +0530

    Time: 0 ms (100%) | Memory: 50.5 MB (44.55%) - LeetCommit

---
+++ Algorithms/4390-Minimum-Bishop-Moves-to-Reach-Target/Minimum-Bishop-Moves-to-Reach-Target.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    
+
+    int minBishopMoves(vector<int>& source, vector<int>& target) {
+        int sr = source[0];
+        int sc = source[1];
+
+        int dr = target[0];
+        int dc = target[1];
+
+        if (((sr + sc) % 2) == (dr + dc) % 2) {
+
+        
+            if (abs(sr - dr) == abs(sc - dc)) {
+                return 1;
+            } else {
+                return 2;
+            }
+        } else {
+            return -1;
+        }
+    }
+};
